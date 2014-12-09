ecb41b7cdd645056fb0c73b5543ad03b2410e7ae
magicshui <shuiyuzhe@gmail.com>
2014-12-09 21:27:42 +0800

    提交修改后的版本

--- mongodb/dbauth.go
+++ mongodb/dbauth.go
@@ -11,27 +11,58 @@ var dbAuthSession map[string]*mgo.Session
 func GetAuthMongodbSession(hosts string, user string, password string, db string) (*mgo.Session, error) {
 	if dbAuthSession == nil {
 		dbAuthSession = make(map[string]*mgo.Session)
+		goto GETSESSION
 	}
 	s := dbAuthSession[db]
 	if s == nil {
-		fmt.Println("初始化数据库链接")
-		var err error
-		dial := new(mgo.DialInfo)
-		fmt.Println(strings.Split(hosts, ","))
-		dial.Addrs = strings.Split(hosts, ",")
-		dial.Username = user
-		dial.Password = password
-		dial.Database = db
-		s, err = mgo.DialWithInfo(dial)
-		if err != nil {
-			panic(err)
-			return nil, err
+		goto GETSESSION
+	} else {
+		if s.Ping() != nil {
+			return s.Clone(), nil
 		}
-		dbAuthSession[db] = s
-
+		goto GETSESSION
 	}
+GETSESSION:
+	var err error
+	dial := new(mgo.DialInfo)
+	dial.Addrs = strings.Split(hosts, ",")
+	dial.Username = user
+	dial.Password = password
+	dial.Database = db
+	s, err = mgo.DialWithInfo(dial)
+	if err != nil {
+		panic(err)
+		return nil, err
+	}
+	dbAuthSession[db] = s
 	return s.Clone(), nil
+
 }
+
+/*
+	func GetAuthMongodbSession(hosts string, user string, password string, db string) (*mgo.Session, error) {
+		if dbAuthSession == nil {
+			dbAuthSession = make(map[string]*mgo.Session)
+		}
+		s := dbAuthSession[db]
+		if s == nil {
+			var err error
+			dial := new(mgo.DialInfo)
+			dial.Addrs = strings.Split(hosts, ",")
+			dial.Username = user
+			dial.Password = password
+			dial.Database = db
+			s, err = mgo.DialWithInfo(dial)
+			if err != nil {
+				panic(err)
+				return nil, err
+			}
+			dbAuthSession[db] = s
+
+		}
+		return s.Clone(), nil
+	}
+*/
 func GetAuthCollectionsWithName(name string, db string, session *mgo.Session) *mgo.Collection {
 	c := session.DB(db).C(name)
 	return c
