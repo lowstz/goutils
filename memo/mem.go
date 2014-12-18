6e116e16d92332952f0af7157e9acab848ec5e08
magicshui <shuiyuzhe@gmail.com>
2014-12-18 17:56:40 +0800

    更新memcache 的代码

--- memo/mem.go
+++ memo/mem.go
@@ -4,14 +4,21 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
+var mc *memcache.Client
+
 func SetMemcache(k string, v string, host string) {
-	mc := memcache.New(host)
+	if mc == nil {
+		mc = memcache.New(host)
+	}
 	mc.Set(&memcache.Item{Key: k, Value: []byte(v)})
 
 }
 
 func GetMemcache(k string, host string) string {
-	mc := memcache.New(host)
+	if mc == nil {
+		mc = memcache.New(host)
+
+	}
 	it, err := mc.Get(k)
 	if err != nil {
 		return ""
